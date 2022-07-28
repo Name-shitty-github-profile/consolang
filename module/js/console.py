c8b6b63af6b64fc8c07a56ab1b361c26c64b3710
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 00:37:16 -0400

    Update console.py

--- module/js/console.py
+++ module/js/console.py
@@ -4,11 +4,13 @@ from .clear import clear
 from .debug import debug
 from .error import error
 from .count import count, countReset
+from .warn import warn
 class console:
-  info = log = log
+  dirxml = info = log = log
   assertt = assertt
   clear = clear
   debug = debug
   error = error
   count = count
   countReset = countReset
+  warn = warn
