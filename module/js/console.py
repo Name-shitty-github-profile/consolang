40e86ee2c2dc8265c950632535ad104523f9bdd5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 00:06:24 -0400

    Update console.py

--- module/js/console.py
+++ module/js/console.py
@@ -3,9 +3,12 @@ from .assertt import assertt
 from .clear import clear
 from .debug import debug
 from .error import error
+from .count import count, countReset
 class console:
-  log = log
+  info = log = log
   assertt = assertt
   clear = clear
   debug = debug
   error = error
+  count = count
+  countReset = countReset
