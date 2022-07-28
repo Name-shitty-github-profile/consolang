0e188eb681a040af9bd177970e288865de388564
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 01:27:27 -0400

    Update error.py

--- module/js/error.py
+++ module/js/error.py
@@ -2,7 +2,9 @@ class Error(Exception):
   pass
 
 def error(message: any, *optionalParams):
+  '''
+  Represent the `console.error()`, raise an the Error Exception when with the message you want to display
+  '''
   message = str(message)
-  message.replace('%d', '')
   for i in optionalParams: message += f'\n{str(i)}'
   raise Error(message)
