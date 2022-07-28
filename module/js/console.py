5d37a7488e732aaefd8dabf9391dbcf2227d5da9
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-27 22:08:54 -0400

    Create console.py

---
+++ module/js/console.py
@@ -0,0 +1,11 @@
+from .log import log
+from .assertt import assertt
+from .clear import clear
+from .debug import debug
+from .error import error
+class console:
+  log = log
+  assertt = assertt
+  clear = clear
+  debug = debug
+  error = error
