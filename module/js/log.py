1e07b1506b090ad1978d985349c1a15c93529454
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 01:03:28 -0400

    Update log.py

--- module/js/log.py
+++ module/js/log.py
@@ -1,6 +1,14 @@
 from sys import stdout
 def log(message: any, *optionalParams) -> None:
+  '''
+  Represent the console.log of javascript work the same as javascript except it's not really formated because to learn python you need to use python formating a bit
+  Only print the character in the params, here's an example
+  ```python
+  > console.log("Hello, world !");
+  > Hello, world !
+  > console.log("Hello,", " world !")
+  > Hello, world !
+  '''
   message = str(message)
-  message.replace('%d', '')
   for i in optionalParams: message += f' {str(i)}'
   stdout.write(message + '\n')
