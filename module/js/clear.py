4a2e82b8be6a35b2c51d27487e7eec34b89ca9ba
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 01:13:55 -0400

    Update clear.py

--- module/js/clear.py
+++ module/js/clear.py
@@ -1,3 +1,6 @@
 from os import system
 def clear() -> None:
+  """
+  Represent the `console.clear()` javascript function, clear the console, run the clear command in the shell
+  """
   system('clear')
