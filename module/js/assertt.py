b070c124ed7f14aedb82c6f4fccabfb76f861a99
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 01:19:30 -0400

    Update assertt.py

--- module/js/assertt.py
+++ module/js/assertt.py
@@ -7,6 +7,15 @@ def assertt(value: bool, message: any = "Assertion failed", *optionalParams) -> 
 console.assert() writes a message if value is falsy or omitted. It only writes a message and does not otherwise affect execution. The output always starts with "Assertion failed". If provided, message is will be displayed in the console.<br>
 
 If value is truthy, nothing happens.
+```python
+console.assert(true, 'does nothing');
+
+console.assert(false, 'Whoops %s work', 'didn\'t');
+# Assertion failed: Whoops didn't work
+
+console.assert();
+# Assertion failed
+```
   '''
   if not value:
     log(message if message == "Assertion failed" else "Assertion failed" + message, *optionalParams)
