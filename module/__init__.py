9e218c36f5bfc19c4e6024d9252c505948cd555d
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 00:08:04 -0400

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -1,9 +1,1 @@
-from .log import log
-from .assertt import assertt
-from .clear import clear
-from .debug import debug
-class console:
-  log = log
-  assertt = assertt
-  clear = clear
-  debug = debug
+from . import js
