2259729eba30f9f5e8ffe11015e66b9fd4fb7946
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-28 01:22:34 -0400

    Update assertt.py
    Fixing indentation and making it more like js.

--- module/js/assertt.py
+++ module/js/assertt.py
@@ -1,21 +1,21 @@
 from .log import log
-def assertt(value: bool, message: any = "Assertion failed", *optionalParams) -> None:
+def assertt(value: bool = False, message: any = "Assertion failed", *optionalParams) -> None:
   '''
   Represent `console.assert()`<br>
   The value tested for being truthy.<br>
 
-console.assert() writes a message if value is falsy or omitted. It only writes a message and does not otherwise affect execution. The output always starts with "Assertion failed". If provided, message is will be displayed in the console.<br>
+  console.assert() writes a message if value is falsy or omitted. It only writes a message and does not otherwise affect execution. The output always starts with "Assertion failed". If provided, message is will be displayed in the console.<br>
 
-If value is truthy, nothing happens.
-```python
-console.assert(true, 'does nothing');
+  If value is truthy, nothing happens.
+  ```python
+  console.assert(true, 'does nothing');
 
-console.assert(false, 'Whoops %s work', 'didn\'t');
-# Assertion failed: Whoops didn't work
+  console.assert(false, 'Whoops %s work', 'didn\'t');
+  # Assertion failed: Whoops didn't work
 
-console.assert();
-# Assertion failed
-```
+  console.assert();
+  # Assertion failed
+  ```
   '''
   if not value:
     log(message if message == "Assertion failed" else "Assertion failed" + message, *optionalParams)
